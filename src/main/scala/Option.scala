1d3132966a25ed01da9323cf7d0dbc7b145dae7c
Alessandro Venturini <ales.ventus21@gmail.com>
2025-07-12 09:49:38 +0200

    add example for lists

--- src/main/scala/Option.scala
+++ src/main/scala/Option.scala
@@ -67,3 +67,17 @@ def testMyFuture: Unit =
   }
   computation2.run(exec)
   exec.close()
+
+given CpsMonad[List] with CpsPureMonadInstanceContext[List] with
+  def pure[T](t: T): List[T] = List(t)
+  def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
+  def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] = fa.flatMap(f)
+
+@main
+def testList: Unit =
+  println:
+    async[List] {
+      val a = List("1", "2", "3").await
+      val b = List("1", "2", "3").await
+      a + b
+    }
